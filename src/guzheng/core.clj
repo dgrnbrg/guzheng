e0813c23107d2940e8e4d601ed335982aef15f20
David Greenberg <dsg123456789@gmail.com>
2012-04-06 02:32:39 -0400

    Added branch tracking code
    This creates an atom per namespace and that tracks which branches were
    taken. Another function is defined to query this atom after however
    many executions.

--- src/guzheng/core.clj
+++ src/guzheng/core.clj
@@ -6,7 +6,7 @@
   [s]
   (-> s
     java.io.StringReader.
-    java.io.PushbackReader.))
+    clojure.lang.LineNumberingPushbackReader.))
 
 (defn seqable?
   "Returns true if (seq x) will succeed, false otherwise."
@@ -33,18 +33,85 @@
     (in-ns old-ns)
     result))
 
+(defn trace-ifs
+  [f ast]
+  (postwalk #(if (and (seqable? %) (= (first %) 'if)) (f %) %) ast))
+
+;(require 'clojure.pprint)
+
+(defn print-trace-if
+  [ast]
+  `(let [x# ~(second ast)]
+     (println (str "if statement on line "
+                   ~(-> ast meta :line)
+                   " is " x#))
+     (if x# ~@(nnext ast))))
+
+(def ^:dynamic *trace-id*)
+(defn branchdetect-if
+  "Takes an ast of an if and returns
+  the transformed AST, having added its
+  info to the *trace-atom* and adding
+  a call to update its data from the
+  *trace-atom* when the branch gets executed."
+  [a ast]
+  (let [id @*trace-id*]
+    (swap! *trace-id* inc)
+    (clojure.pprint/pprint [:ast :is ast])
+    `(do
+       (when-not (get @~a ~id)
+         (swap! ~a
+                assoc
+                ~id
+                {:line ~(-> ast meta :line)
+                 :ast '~ast
+                 :lhs 0
+                 :rhs 0})) 
+       (if ~(second ast)
+         (let [lhs# (get-in @~a [~id :lhs])]
+           (swap! ~a
+                  assoc-in
+                  [~id :lhs]
+                  (inc lhs#))
+           ~(nth ast 2))   
+         (let [rhs# (get-in @~a [~id :rhs])]
+           (swap! ~a
+                  assoc-in
+                  [~id :rhs]
+                  (inc rhs#))
+           ~(nth ast 3))))))
+
+(defn trace-if-branches
+  "a is an atom that'll contain the instrumentation
+  data."
+  [a ast]
+  (binding [*trace-id* (atom 0)]
+    (let [trace-atom (gensym)
+          new-ast
+          (postwalk 
+            (fn [node]
+              (if (seqable? node)
+                (condp = (first node)
+                  'if (branchdetect-if trace-atom node)
+;                  'defn (concat node
+;                                `( (clojure.pprint/pprint
+;                                   [:trace ~trace-atom])))
+                  node)
+                node))
+            ast)
+          new-ast (concat new-ast `((defn ~'trace-result [] 
+                                      [:trace ~trace-atom])))]
+      (clojure.pprint/pprint new-ast)
+      `(let [~trace-atom (atom {})]
+         ~new-ast))))
+
+(def a (atom {}))
 (instrument "
             ;go fred!
             (ns fred.rules)
             (defn hello-world [] (println \"hello world2\")))
             "
-            (partial postwalk
-                     (fn [node]
-                       (if (and (seqable? node)
-                                (= (first node) 'println))
-                         (concat '(do (println "calling println"))
-                                 node)
-                         node))))
+            (partial trace-if-branches a))
 
 #_(alter-var-root #'clojure.core/load
                 (fn [old-load]
