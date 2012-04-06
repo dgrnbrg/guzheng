0095cce4804b46aaae0fc7e3b421cb5418f49e99
David Greenberg <dsg123456789@gmail.com>
2012-04-06 18:41:55 -0400

    Did first push to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject guzheng/guzheng "1.0.0-SNAPSHOT"
+(defproject guzheng/guzheng "1.0.0"
   :description "guzheng: an instrumentation and code coverage library for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
