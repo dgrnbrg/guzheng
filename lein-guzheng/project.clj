c1c619b9fede40615bbdfd45bfae4eacec844ddd
David Greenberg <dsg123456789@gmail.com>
2012-10-01 22:37:26 -0400

    Move guzheng to lein2

--- lein-guzheng/project.clj
+++ lein-guzheng/project.clj
@@ -3,9 +3,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[guzheng ~(nth (read-string (slurp "../project.clj")) 2)]
-                 [bultitude "0.1.5"]
-                 [lein-sleight "0.2.0-SNAPSHOT"]
-                 [robert/hooke "1.1.3"]
-                 [leinjacker "0.3.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dependencies [[lein-sleight "0.2.0-SNAPSHOT"]
+                 [leinjacker "[0.3.0,)"]]
+  :plugins [[lein-clojars "0.6.0"]])
--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [bultitude "0.1.5"]
                  [org.clojure/core.logic "0.7.5"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :plugins [[lein-clojars "0.6.0"]])
