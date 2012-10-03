470c38159ab3bd3e991c7ba69f82141d1cc32377
David Greenberg <dsg123456789@gmail.com>
2012-10-02 21:43:37 -0400

    Correctly work around lein2 profile unmerging for sleight task chaining

--- lein-guzheng/src/leiningen/guzheng.clj
+++ lein-guzheng/src/leiningen/guzheng.clj
@@ -1,7 +1,7 @@
 (ns leiningen.guzheng
   (:use clojure.pprint)
-  (:require leiningen.test)
-  (:require [leiningen.sleight :as sleight])
+  (:require leiningen.test
+            [leiningen.core.project :as project])
   (:require [leinjacker.eval :as eval])
   (:require [clojure.java.io :as io]))
 
@@ -14,22 +14,28 @@
   by the leiningen command to run with an instrumented
   eval-in-project."
   [args]
-  (let [[nses subtask] (split-with #(not= "--" %) args)]
+  (let [[nses [_ & subtask]] (split-with #(not= "--" %) args)]
     [nses subtask]))
+
+(defn update-project-dependencies
+  [project deps]
+  (let [profile-name (-> (gensym) name keyword)
+        added-profile (project/add-profiles project
+                                            {profile-name deps})
+        merged-profile (project/merge-profiles added-profile [profile-name])]
+    merged-profile))
 
 (defn guzheng
   "Takes a list of namespaces followed by -- and
   another leiningen task and executes that task
   with the given namespaces instrumented."
   [project & args]
-  (let [[nses [_ subtask & sub-args]] (split-ns-subtask args)
+  (let [[nses [subtask & sub-args]] (split-ns-subtask args)
         project (-> project
-                  (update-in [:dependencies] conj guzheng-version)
-                  (assoc-in [:sleight :guzheng]
-                             {:transforms ['guzheng.core/instrument]
-                              :namespaces (vec nses)}))
+                  (update-project-dependencies
+                    {:dependencies [guzheng-version]
+                     :sleight {:guzheng {:transforms ['guzheng.core/instrument]
+                                         :namespaces (vec nses)}}}))
         task-args (concat [":guzheng" subtask] sub-args)]
-    (pprint project)
-    (println "calling sleight with" task-args)
     (binding [leiningen.test/*exit-after-tests* false]
       (eval/apply-task "sleight" project task-args))))
